21f3ec5ff3c19f5ad4c4d5a39ec226f2c0f3e64c
Jason Madar <env3d@users.noreply.github.com>
2025-09-01 19:16:35 +0000

    update tests and workflow

--- tests/test_assignments.py
+++ tests/test_assignments.py
@@ -1,16 +1,29 @@
 import pathlib
 import os
 
-files = ['TSLA.csv', 'tesla.db', 'schema.sql', 'import.sql']
+def test_TSLA_csv_exists():
+    assert pathlib.Path('TSLA.csv').is_file()
 
-def test_files_exist():
-    # Test for the presence of various files
-    for f in files:
-        assert pathlib.Path(f).is_file()
+def test_tesla_db_exists():
+    assert pathlib.Path('tesla.db').is_file()
 
-def test_file_size():
-    for f in files:
-        assert pathlib.Path(f).stat().st_size > 0
+def test_schema_sql_exists():
+    assert pathlib.Path('schema.sql').is_file()
+
+def test_import_sql_exists():
+    assert pathlib.Path('import.sql').is_file()
+
+def test_TSLA_csv_size():
+    assert pathlib.Path('TSLA.csv').stat().st_size > 0
+
+def test_tesla_db_size():
+    assert pathlib.Path('tesla.db').stat().st_size > 0
+
+def test_schema_sql_size():
+    assert pathlib.Path('schema.sql').stat().st_size > 0
+
+def test_import_sql_size():
+    assert pathlib.Path('import.sql').stat().st_size > 0
 
 
 
